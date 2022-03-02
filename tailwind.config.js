e069be12ea256f6408714083e4f1a19a5a112674
Sarah <sarisp3260@gmail.com>
2022-03-02 09:43:38 -0500

    First commit & tailwind already configurated :tada:

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        purple: "#B4A4FC",
+        primary: "#6951EB",
+        blueDark: "#1B2B43",
+        white: "#F9F2F5"
+      }
+    },
+  },
+  plugins: [],
+}
